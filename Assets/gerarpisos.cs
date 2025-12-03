b21bd14b5788bb779bf5e0c89cc4d20b2905b4f4
CESAR_C# <lianbastiam@gmail.com>
2025-12-03 17:07:42 -0300

    fixed

--- Assets/gerarpisos.cs
+++ Assets/gerarpisos.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class gerarpisos : MonoBehaviour
@@ -8,18 +9,25 @@ public class gerarpisos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("gerarlospisos");
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine("gerarlospisos");
+
     }
 
     IEnumerator gerarlospisos()
     {
-        yield return new WaitForSeconds(2);
-        GameObject clonePiso = Instantiate(target, target.transform.position, target.transform.rotation);
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            float valor = UnityEngine.Random.Range(1, 10);
+            if (valor >= 7)
+            {
+                GameObject clonePiso = Instantiate(target, target.transform.position, target.transform.rotation);
+            }
+        }
     }
 }
---
+++ Assets/obtaclescript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obtaclescript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            // morte
+        }
+    }
+}
