d91b6edd5aca1f59e0e1b5c5a644984ac522ba45
CCesar-c <lianbastiam@gmail.com>
2025-12-08 10:55:24 -0300

    fixed

--- Assets/scripts/moviment_fondo.cs
+++ Assets/scripts/moviment_fondo.cs
@@ -6,7 +6,7 @@ public class moviment_fondo : MonoBehaviour
 {
     public float force = 10;
     GameObject player;
-    // Start is called before the first frame update
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
